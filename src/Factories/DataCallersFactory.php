fe1c8e0dcfb39720dfa18d6ca60b8edad36eea5b
Carlo Nicora <carlo.nicora@gmail.com>
2020-05-25 13:13:43 +0100

    DataCallerFacade creation simplified

--- src/Factories/DataCallersFactory.php
+++ src/Factories/DataCallersFactory.php
@@ -4,13 +4,16 @@ namespace CarloNicora\Minimalism\Services\Data\Factories;
 use CarloNicora\Minimalism\Core\Services\Factories\ServicesFactory;
 use CarloNicora\Minimalism\Services\Cacher\Interfaces\CacheFactoryInterface;
 use CarloNicora\Minimalism\Services\Data\Facade\DataCallerFacade;
-use CarloNicora\Minimalism\Services\MySQL\Interfaces\TableInterface;
+use CarloNicora\Minimalism\Services\MySQL\MySQL;
 use Exception;
 
 class DataCallersFactory
 {
     /** @var ServicesFactory  */
     private ServicesFactory $services;
+
+    /** @var MySQL  */
+    private MySQL $mysql;
 
     /**
      * DataCallersFactory constructor.
@@ -19,10 +22,11 @@ class DataCallersFactory
     public function __construct(ServicesFactory $services)
     {
         $this->services = $services;
+        $this->mysql = $services->service(MySQL::class);
     }
 
     /**
-     * @param TableInterface $table
+     * @param string $tableName
      * @param string $functionName
      * @param array $functionParameters
      * @param CacheFactoryInterface|null $dataCache
@@ -30,12 +34,13 @@ class DataCallersFactory
      * @throws Exception
      */
     public function create(
-        TableInterface $table,
+        string $tableName,
         string $functionName,
         array $functionParameters = [],
         CacheFactoryInterface $dataCache = null
     ) : DataCallerFacade
     {
+        $table = $this->mysql->create($tableName);
         return new DataCallerFacade(
             $this->services,
             $table,
